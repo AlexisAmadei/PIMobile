0cd8f02c8c204411281a1397e57671d1b56e5ac2
Alexis Amadei <alexisamadei@gmail.com>
2023-04-15 23:47:39 +0200

    update

--- pim-2023/src/views/HomePage.js
+++ pim-2023/src/views/HomePage.js
@@ -1,26 +1,31 @@
 import React from "react";
 
-import { updateDoc, doc } from "firebase/firestore";
-import { db, auth } from "../config/firebaseConfig";
-import { signOut } from "firebase/auth";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
+import logoText from "../assets/logoText.svg";
+import groupSquares from "../assets/groupSquares.svg";
+import iconChatApp from "../assets/iconChatApp.svg";
+import iconProfile from "../assets/iconProfile.svg";
 
 export default function HomePage() {
-    const logout = async () => {
-        try {
-            await signOut(auth);
-        } catch (err) { console.error(err); }
-    };
-    const chatApp = () => {
-        console.log("redirect chatApp");
-    }
-    return (
-        <div>
-            <h1>HomePage</h1>
-            <button onClick={logout}>Logout</button>
-            <Link to="/chatApp">Chat App</Link>
-            <Link to="/profile">Profile</Link>
+  return (
+    <div className="globalHomeContainer">
+      <div className="homeContainer">
+        <div className="headerHome">
+          <div className="headerLeft">
+            <img src={groupSquares} />
+          </div>
+          <div className="headerCenter">
+            <img src={logoText} />
+          </div>
+          <div className="headerRight">
+            <img src={iconChatApp} />
+            <img src={iconProfile} />
+          </div>
         </div>
-    );
+      </div>
+      <Link to="/chatApp">Chat App</Link>
+      <Link to="/profile">Profile</Link>
+    </div>
+  );
 }
