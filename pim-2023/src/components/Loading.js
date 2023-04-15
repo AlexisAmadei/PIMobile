fa0360dfd3e6c1c2185383501b48bf50efd82b57
Alexis Amadei <alexisamadei@gmail.com>
2023-04-15 10:34:39 +0200

    change loading component

--- pim-2023/src/components/Loading.js
+++ pim-2023/src/components/Loading.js
@@ -1,33 +1,13 @@
 import React from 'react';
+import '../css/Loading.css';
 
-export default function LoadingIndicator() {
-    const LoadingIndicator = {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        top: '0',
-        left: '0',
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        zIndex: '9999',
-    }
-    const spinnerBorder = {
-        width: '3rem',
-        height: '3rem',
-        borderWidth: '0.25rem',
-        borderColor: 'rgba(255, 255, 255, 0.5)',
-        borderRadius: '50%',
-        borderTopColor: '#fff',
-        animation: 'spinner-border 1s linear infinite',
-    }
-    return (
-        <div style={LoadingIndicator}>
-            <span className="sr-only">Loading...</span>
-            <div style={spinnerBorder} role="status">
-                <span className="sr-only">Loading...</span>
-            </div>
-        </div>
-    );
-}
+const LoadingPage = () => {
+  return (
+    <div className="loading-page">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default LoadingPage;
